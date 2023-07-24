3567bfed77cfd6bc793fd0d3f3af7ce2f05f7da2
Gar-Yin Lam <garyin.lam@gmail.com>
2023-07-24 13:45:52 +0100

    Table component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,142 @@
+
+import {
+  getPaginationRowModel,
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+
+const columnHelper = createColumnHelper()
+
+const columns = [
+  columnHelper.accessor('id', {
+    header: 'ID',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('icon_uri', {
+    header: 'Image',
+    cell: info => <div><img height={34} src={info.getValue()}/></div>,
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('name.name-USen', {
+    header: 'Name',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('species', {
+    header: 'Species',
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('catch-phrase', {
+    header: 'Catch-phrase',
+    footer: info => info.column.id,
+  }),
+]
+
+
+const Table = ({data}) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+          pageSize: 20,
+      },
+  },
+  })
+
+  return (
+    <div>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <button
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span>
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span>
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[20, 40, 80].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default Table
