3d1b35a7be420f7f91b5e05f5505a5da964418db
Gar-Yin Lam <garyin.lam@gmail.com>
2023-07-24 20:11:50 +0100

    header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
+import "./Header.scss"
 
 const Header = () => {
   return (
-    <header>
-      <img src="https://www.animal-crossing.com/new-horizons/assets/img/global/logos/logo-acnh-en.png" alt="Animal Crossing New Horizons Logo"/>
-      <h1>Animal Crossing New Horizons Villager List</h1>
+    <header className="header">
+      <img src="https://www.animal-crossing.com/new-horizons/assets/img/global/logos/logo-acnh-en.png" alt="Animal Crossing New Horizons Logo" className="header__image"/>
+      <h1 className="header__text">Animal Crossing New Horizons Villager List</h1>
     </header>
   )
 }
