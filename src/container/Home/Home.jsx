75b6c176869121c996ee2f98d020489d58a35ff4
Gar-Yin Lam <garyin.lam@gmail.com>
2023-07-24 16:45:30 +0100

    Home

---
+++ src/container/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Header from '../../components/Header/Header'
+import Table from '../../components/Table'
+
+const Home = () => {
+
+  const defaultData  = [
+    {
+      id: '',
+      name: {
+        "name-USen": ''
+      },
+      "image-uri": "",
+      species: "",
+      "catch-phrase": '',
+    },
+  ]
+
+  const [data, setData] = useState(() =>[...defaultData]);
+
+  const getVillagers = async () => {
+    const res = await fetch(
+      `https://acnhapi.com/v1/villagers/`
+    );
+    const data = await res.json();
+    const dataArray = Object.values(data)
+    setData(dataArray);
+    console.log(dataArray);
+  };
+
+  useEffect(() => {
+    getVillagers();
+  }, []);
+
+  return (
+    <div>
+      <Header/>
+      <Table data={data}/>
+    </div>
+  )
+}
+
+export default Home
